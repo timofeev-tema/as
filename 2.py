cc99d51dff8193bbe575827f27b984a1b3d83485
timofeev-tema <timov000@mail.ru>
2024-08-20 17:37:00 +0300

    Add files via upload

---
+++ 2.py
@@ -0,0 +1,5 @@
+total_tasks = 12
+hours_spent = 1.5
+average_time = 1.5/12
+title= 'Python'
+print ('Курс: ',title ,", всего задач: " , total_tasks , ", затрачено часов" , hours_spent , ", среднее время выполнения" , average_time , "часа." )
